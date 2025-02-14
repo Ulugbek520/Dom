c7e8015943f1fa8d2145336ff7904eed9837ec2a
ulug'bek <azizbekovulugbek8@gmail.com>
2025-02-14 11:12:00 +0500

    none

---
+++ index.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector("h1");
+let ism = prompt("Ismingizni kiriting: ");
+let rang = prompt("Yoqtirgan rangizni")
+let bg = prompt("Orga fon rangi: ");
+
+h1.textContent = `hello, ${ism}!`;
+h1.style.color = rang;
+h1.style.backgroundColor = bg;
+h1.style.textAligh = "center";
